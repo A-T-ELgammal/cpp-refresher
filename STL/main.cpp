3eac0eaa54eb57f58373970f5ffcd49b46cfc929
atelgammal <a.t.elgammal@gmail.com>
2024-05-28 01:25:34 +0300

    iterate through map with example of character count in a string

--- STL/main.cpp
+++ STL/main.cpp
@@ -22,9 +22,26 @@ int main()
     mp.insert(p1);
     cout << mp['K'] << endl;
     cout << p1.first << "  " << p1.second << endl;
-    mp.clear();
+    // mp.clear();
     cout << mp.size() << endl;
     cout << mp.empty() << endl; // return 1 for true
-    
+    cout << "////////////////////////////" << endl;
+    // iterate through the map
+    for (map<char, int>::iterator itr = mp.begin(); itr != mp.end(); ++itr)
+    {
+        cout << itr->first << "  ";
+        cout << itr->second << endl;
+    }
+    cout << "////////////////////////////" << endl;
+
+    // count the character in a string example
+    string test = {"Hello World my name is ahmed! elgammal tttthhhhaaaa"};
+    map<char, int> characterCount;
+    for (char c : test)
+        characterCount[c]++;
+
+    for (map<char, int>::iterator i = characterCount.begin(); i != characterCount.end(); ++i)
+        cout << i->first << "  " << i->second << endl;
+
     return 0;
 }
